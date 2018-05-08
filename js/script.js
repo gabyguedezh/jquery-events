2bfa9759e661da3591379b4b74dfdd91b725e40b
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-08 15:17:10 +0000

    initial commit. Challenge solved. Help from the solutions but they made sense

---
+++ js/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+	$("p").click(function() {
+		$("p").addClass("change-to-red");
+	});
+	$("h2").hover(function() {
+		$("h2").addClass("lightblue-effect");
+	});
+	// $("#hr_html").hover(function() {
+	// 	$("#hr_html").addClass("hr_html_hover");
+	// });
+	 $("#hr_html").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_html").addClass("h2_font_size");
+
+    });
+
+    $("#hr_mysql").hover(function(){
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_mysql").addClass("h2_font_size");
+    });
+
+    $("#hr_python").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_python").addClass("h2_font_size");
+    });
+
+    $("#hr_jquery").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_jquery").addClass("h2_font_size");
+    });
+
+    $("#hr_django").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_css").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_django").addClass("h2_font_size");
+    });
+
+    $("#hr_css").hover(function(){
+        $("#hr_mysql").removeClass("h2_font_size");
+        $("#hr_python").removeClass("h2_font_size");
+        $("#hr_jquery").removeClass("h2_font_size");
+        $("#hr_django").removeClass("h2_font_size");
+        $("#hr_html").removeClass("h2_font_size");
+        $("#hr_css").addClass("h2_font_size");
+    });
+	// $(".bottom_button").mouseenter(function() {
+	// 	$("body").addClass(".mouseenter-background");
+	// });
+	// $(".bottom_button").mouseleave(function() {
+	// 	$("body").addClass(".mouseleave-background");
+	// });
+	$(".bottom_button").mouseenter(function() {
+		$("body").css("background-color", "black");
+	});
+	$(".bottom_button").mouseleave(function() {
+		$("body").css("background-color", "#eee");
+	});
+});
