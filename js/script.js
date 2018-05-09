c53db7320529c75c5a233e9ec8e28a7d650cb6df
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-09 09:39:46 +0000

    TOGGLE SLIDE challenge and MOUSEENTER/MOUSELEAVE challenges completed. Tutor checked and said the code was well

--- js/script.js
+++ js/script.js
@@ -76,24 +76,71 @@ $(document).ready(function() {
     $(".bottom_button").mouseleave(function() {
         $("body").css("background-color", "#eee");
     });
+    
+    // Code below belongs to the HIDE challenge and it's now commented to avoid clashing effect with TOGGLE challenge
+
+    // $("#first_button").click(function() {
+    //     $("#first_button").hide("slow");
+    // });
 
     $("#first_button").click(function() {
-        $("#first_button").hide("slow");
+        $("#first_para").slideToggle("slow");
     });
-
     $("#second_button").click(function() {
-        $("#second_para").toggle("slow");
+        $("#second_para").slideToggle("slow");
     });
     $("#third_button").click(function() {
-        $("#third_para").toggle("slow");
+        $("#third_para").slideToggle("slow");
     });
     $("#fourth_button").click(function() {
-        $("#fourth_para").toggle("slow");
+        $("#fourth_para").slideToggle("slow");
     });
     $("#fifth_button").click(function() {
-        $("#fifth_para").toggle("slow");
+        $("#fifth_para").slideToggle("slow");
     });
     $("#sixth_button").click(function() {
-        $("#sixth_para").toggle("slow");
-    });    
+        $("#sixth_para").slideToggle("slow");
+    });
+    
+    $("#first_button").mouseenter(function(){
+        $("#first_button").fadeTo("slow", 0.5);
+    });
+    $("#first_button").mouseleave(function(){
+        $("first_button").fadeTo("slow", 1);
+    });  
+    
+    $("#second_button").mouseenter(function(){
+        $("#second_button").fadeTo("slow", 0.5);
+    });
+    $("#second_button").mouseleave(function(){
+        $("#second_button").fadeTo("slow", 1);
+    }); 
+    
+    $("#third_button").mouseenter(function(){
+        $("#third_button").fadeTo("slow", 0.5);
+    });
+    $("#third_button").mouseleave(function(){
+        $("#third_button").fadeTo("slow", 1);
+    }); 
+    
+    $("#fourth_button").mouseenter(function(){
+        $("#fourth_button").fadeTo("slow", 0.5);
+    });
+    $("#fourth_button").mouseleave(function(){
+        $("#fourth_button").fadeTo("slow", 1);
+    }); 
+    
+    $("#fifth_button").mouseenter(function(){
+        $("#fifth_button").fadeTo("slow", 0.5);
+    });
+    $("#fifth_button").mouseleave(function(){
+        $("#fifth_button").fadeTo("slow", 1);
+    }); 
+    
+    $("sixth_button").mouseenter(function(){
+        $("sixth_button").fadeTo("slow", 0.5);
+    });
+    $("sixth_button").mouseleave(function(){
+        $("sixth_button").fadeTo("slow", 1);
+    });     
 });
