94a83359529dcc699bfa264207ec245d55e4b2db
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-09 09:10:47 +0000

    toggle button challenge completed. Hide challenge commented

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-	$("p").click(function() {
-		$("p").addClass("change-to-red");
-	});
-	$("h2").hover(function() {
-		$("h2").addClass("lightblue-effect");
-	});
-	// $("#hr_html").hover(function() {
-	// 	$("#hr_html").addClass("hr_html_hover");
-	// });
-	 $("#hr_html").hover(function(){
+    $("p").click(function() {
+        $("p").addClass("change-to-red");
+    });
+    $("h2").hover(function() {
+        $("h2").addClass("lightblue-effect");
+    });
+    // $("#hr_html").hover(function() {
+    // 	$("#hr_html").addClass("hr_html_hover");
+    // });
+    $("#hr_html").hover(function() {
         $("#hr_mysql").removeClass("h2_font_size");
         $("#hr_python").removeClass("h2_font_size");
         $("#hr_jquery").removeClass("h2_font_size");
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
     });
 
-    $("#hr_mysql").hover(function(){
+    $("#hr_mysql").hover(function() {
         $("#hr_python").removeClass("h2_font_size");
         $("#hr_jquery").removeClass("h2_font_size");
         $("#hr_django").removeClass("h2_font_size");
@@ -29,7 +29,7 @@ $(document).ready(function() {
         $("#hr_mysql").addClass("h2_font_size");
     });
 
-    $("#hr_python").hover(function(){
+    $("#hr_python").hover(function() {
         $("#hr_mysql").removeClass("h2_font_size");
         $("#hr_jquery").removeClass("h2_font_size");
         $("#hr_django").removeClass("h2_font_size");
@@ -38,7 +38,7 @@ $(document).ready(function() {
         $("#hr_python").addClass("h2_font_size");
     });
 
-    $("#hr_jquery").hover(function(){
+    $("#hr_jquery").hover(function() {
         $("#hr_mysql").removeClass("h2_font_size");
         $("#hr_python").removeClass("h2_font_size");
         $("#hr_django").removeClass("h2_font_size");
@@ -47,7 +47,7 @@ $(document).ready(function() {
         $("#hr_jquery").addClass("h2_font_size");
     });
 
-    $("#hr_django").hover(function(){
+    $("#hr_django").hover(function() {
         $("#hr_mysql").removeClass("h2_font_size");
         $("#hr_python").removeClass("h2_font_size");
         $("#hr_jquery").removeClass("h2_font_size");
@@ -56,7 +56,7 @@ $(document).ready(function() {
         $("#hr_django").addClass("h2_font_size");
     });
 
-    $("#hr_css").hover(function(){
+    $("#hr_css").hover(function() {
         $("#hr_mysql").removeClass("h2_font_size");
         $("#hr_python").removeClass("h2_font_size");
         $("#hr_jquery").removeClass("h2_font_size");
@@ -64,20 +64,36 @@ $(document).ready(function() {
         $("#hr_html").removeClass("h2_font_size");
         $("#hr_css").addClass("h2_font_size");
     });
-	// $(".bottom_button").mouseenter(function() {
-	// 	$("body").addClass(".mouseenter-background");
-	// });
-	// $(".bottom_button").mouseleave(function() {
-	// 	$("body").addClass(".mouseleave-background");
-	// });
-	$(".bottom_button").mouseenter(function() {
-		$("body").css("background-color", "black");
-	});
-	$(".bottom_button").mouseleave(function() {
-		$("body").css("background-color", "#eee");
-	});
-	
-	$("#first_button").click(function(){
-	    $("#first_button").hide("slow");
-	});
+    // $(".bottom_button").mouseenter(function() {
+    // 	$("body").addClass(".mouseenter-background");
+    // });
+    // $(".bottom_button").mouseleave(function() {
+    // 	$("body").addClass(".mouseleave-background");
+    // });
+    $(".bottom_button").mouseenter(function() {
+        $("body").css("background-color", "black");
+    });
+    $(".bottom_button").mouseleave(function() {
+        $("body").css("background-color", "#eee");
+    });
+
+    $("#first_button").click(function() {
+        $("#first_button").hide("slow");
+    });
+
+    $("#second_button").click(function() {
+        $("#second_para").toggle("slow");
+    });
+    $("#third_button").click(function() {
+        $("#third_para").toggle("slow");
+    });
+    $("#fourth_button").click(function() {
+        $("#fourth_para").toggle("slow");
+    });
+    $("#fifth_button").click(function() {
+        $("#fifth_para").toggle("slow");
+    });
+    $("#sixth_button").click(function() {
+        $("#sixth_para").toggle("slow");
+    });    
 });
